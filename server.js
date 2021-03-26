391c1012c408556bcb4073086506f1e85b8655e9
alfinkerin <kerinalfin70@gmail.com>
2021-03-27 04:44:47 +0700

    route

--- server.js
+++ server.js
@@ -6,9 +6,39 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamp" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `get bootcamp ${req.params.id} ` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id} ` });
+});
+
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamp" });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id} ` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE.ENV} mode on port ${PORT}`)
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
